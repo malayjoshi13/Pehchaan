b0dae2700f97720899a159964c2b229f5c2b06fb
malayjoshi13 <13.malayjoshi@gmail.com>
2023-12-19 23:26:44 +0530

    update

--- utils/threshold_fn.py
+++ utils/threshold_fn.py
@@ -6,7 +6,6 @@ def findThreshold(model_name, distance_metric):
 		'VGG-Face': {'cosine': 0.40, 'euclidean': 0.60, 'euclidean_l2': 0.86},
         'Facenet':  {'cosine': 0.40, 'euclidean': 10, 'euclidean_l2': 0.80},
         'ArcFace':  {'cosine': 0.68, 'euclidean': 4.15, 'euclidean_l2': 1.13},
-        'Dlib': 	{'cosine': 0.07, 'euclidean': 0.6, 'euclidean_l2': 0.4},
         'SFace': 	{'cosine': 0.5932763306134152, 'euclidean': 10.734038121282206, 'euclidean_l2': 1.055836701022614},
 		'OpenFace': {'cosine': 0.10, 'euclidean': 0.55, 'euclidean_l2': 0.55},
 		'DeepFace': {'cosine': 0.23, 'euclidean': 64, 'euclidean_l2': 0.64},
